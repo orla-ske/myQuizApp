dea30bfb0e7697bb7058824269d7aef9dfcc3ca5
orla-ske <kodjoorlando@yahoo.co.uk>
2023-08-24 15:48:36 +0000

    First commit

---
+++ src/main/java/com/example/myQuizApp/MyQuizAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.myQuizApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyQuizAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyQuizAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/myQuizApp/MyQuizAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myQuizApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyQuizAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
